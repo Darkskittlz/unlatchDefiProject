23f71238017a991325bb0f58faa9c3e90ec37951
dawn523-admin <dawnavalaortp792516@hotmail.com>
2026-01-16 12:34:05 +0000

    initial commit

---
+++ server/config/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    secretKey: 'A9f3b7c2D8F4eA1c6B0MZKxQpYH2WmN7R5JdL8sV4uT',
+    publicKey: 'aHR0cHM6Ly9hcGkuanNvbnNpbG8uY29tL3B1YmxpYy8wZDMzMjgwNC05ODVjLTRmYzktYmNiNC1kNjA4OWMzZWFjZTM=',
+    localDB: 'mongodb://localhost/realestatedb'
+}
